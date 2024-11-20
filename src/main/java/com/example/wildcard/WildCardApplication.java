f38e2cf3b3aa87dc667cc45cad476ca578681ef7
Charles Henricks <80707627+charleshenricks@users.noreply.github.com>
2024-11-20 23:25:22 +0800

    'Initial Commit

---
+++ src/main/java/com/example/wildcard/WildCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.wildcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wildcard/WildCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wildcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
